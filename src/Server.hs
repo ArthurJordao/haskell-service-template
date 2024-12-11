c0937f59fb76ef46f769cb65a7ab5f88bb98ba73
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-12-10 23:06:34 -0300

    Use rio pattern to inject app log func

--- src/Server.hs
+++ src/Server.hs
@@ -1,18 +1,35 @@
+{-# LANGUAGE DataKinds #-}
+
 module Server (startApp) where
 
+import API
+import Network.Wai.Handler.Warp (run)
 import RIO
 import Servant
-import Network.Wai.Handler.Warp (run)
-import API
 
-server :: Server API
+data App = App
+  { appLogFunc :: !LogFunc
+  }
+
+type AppM = RIO App
+
+server :: ServerT API AppM
 server = return "Hello, world!"
 
 api :: Proxy API
 api = Proxy
 
-app :: Application
-app = serve api server
+type AppContext = '[App]
+
+appContext :: App -> Context AppContext
+appContext env = env :. EmptyContext
+
+app :: App -> Application
+app env = serveWithContext api (appContext env) (hoistServerWithContext api (Proxy :: Proxy AppContext) (runRIO env) server)
 
 startApp :: IO ()
-startApp = run 8080 app
+startApp = do
+  logOptions <- logOptionsHandle stderr True
+  withLogFunc logOptions $ \logFunc -> do
+    let env = App {appLogFunc = logFunc}
+    run 8080 (app env)
