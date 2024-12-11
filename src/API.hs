c89fa33778de5de732f7adb57ea9fe98b3100915
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-12-10 23:33:38 -0300

    Add some sample apis

--- src/API.hs
+++ src/API.hs
@@ -1,8 +1,23 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
-module API where
 
+module API (API, Account(..)) where
+
+import Data.Aeson (FromJSON, ToJSON)
 import RIO
 import Servant
 
-type API = "hello" :> Get '[PlainText] Text
+type API =
+  "status" :> Get '[JSON] Text
+    :<|> "accounts" :> Get '[JSON] [Account]
+    :<|> "accounts" :> Capture "id" Int :> Get '[JSON] Account
+
+data Account = Account
+  { accountId :: Int,
+    accountName :: Text
+  }
+  deriving (Generic)
+
+instance ToJSON Account
+
+instance FromJSON Account
--- src/Server.hs
+++ src/Server.hs
@@ -15,9 +15,31 @@ instance HasLogFunc App where
   logFuncL = lens appLogFunc (\x y -> x {appLogFunc = y})
 
 server :: ServerT API (RIO App)
-server = do
-  logInfo "Api called"
-  return "Hello, world!"
+server =
+  statusHandler
+    :<|> accountsHandler
+    :<|> accountByIdHandler
+
+accounts :: [Account]
+accounts = [Account 1 "Alice", Account 2 "Bob"]
+
+statusHandler :: (HasLogFunc a) => RIO a Text
+statusHandler = do
+  logInfo "Status endpoint called"
+  return "OK"
+
+accountsHandler :: (HasLogFunc a) => RIO a [Account]
+accountsHandler = do
+  logInfo "Accounts endpoint called"
+  return accounts
+
+accountByIdHandler :: (HasLogFunc a) => Int -> RIO a Account
+accountByIdHandler accId = do
+  logInfo $ "Account endpoint called with ID: " <> displayShow accId
+  let account = filter (\acc -> accountId acc == accId) accounts
+  case account of
+    [acc] -> return acc
+    _ -> throwM err404 {errBody = "Account not found"}
 
 api :: Proxy API
 api = Proxy
