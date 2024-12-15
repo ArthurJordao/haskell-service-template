6961f98a0295f21f6917233e45f4f9d3ad6afc57
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-12-15 00:41:06 -0300

    Extract helper function

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,32 +5,31 @@ import RIO
 import Server (App (..), Config (..), app)
 import Test.Hspec
 
+withTestApp :: (Int -> App -> IO ()) -> IO ()
+withTestApp action = do
+  let testConfig = Config {port = 8080, environment = "test"}
+  logOptions <- logOptionsHandle stderr True
+  withLogFunc logOptions $ \logFunc -> do
+    let testApp = App {appLogFunc = logFunc, config = testConfig}
+    testWithApplication (pure $ app testApp) $ \port' -> action port' testApp
+
 spec :: Spec
 spec = describe "Server" $ do
   it "respond with 200 on status" $ do
-    let testConfig = Config {port = 8080, environment = "test"}
-    logOptions <- logOptionsHandle stderr True
-    withLogFunc logOptions $ \logFunc -> do
-      let testApp = App {appLogFunc = logFunc, config = testConfig}
-      testWithApplication (pure $ app testApp) $ \port' -> do
-        manager <- newManager defaultManagerSettings
-        request <- parseRequest ("http://localhost:" <> show port' <> "/status")
-        response <- httpLbs request manager
-        responseStatus response `shouldBe` status200
-        responseBody response `shouldBe` "\"OK\""
+    withTestApp $ \port' _ -> do
+      manager <- newManager defaultManagerSettings
+      request <- parseRequest ("http://localhost:" <> show port' <> "/status")
+      response <- httpLbs request manager
+      responseStatus response `shouldBe` status200
+      responseBody response `shouldBe` "\"OK\""
 
   it "respond with 200 and list of accounts on /accounts" $ do
-    let testConfig = Config {port = 8080, environment = "test"}
-    logOptions <- logOptionsHandle stderr True
-    withLogFunc logOptions $ \logFunc -> do
-      let testApp = App {appLogFunc = logFunc, config = testConfig}
-      testWithApplication (pure $ app testApp) $ \port' -> do
-        manager <- newManager defaultManagerSettings
-        request <- parseRequest ("http://localhost:" <> show port' <> "/accounts")
-        response <- httpLbs request manager
-        responseStatus response `shouldBe` status200
-        -- Assuming the response body is a JSON array of accounts
-        responseBody response `shouldBe` "[{\"accountId\":1,\"accountName\":\"Alice\"},{\"accountId\":2,\"accountName\":\"Bob\"}]"
+    withTestApp $ \port' _ -> do
+      manager <- newManager defaultManagerSettings
+      request <- parseRequest ("http://localhost:" <> show port' <> "/accounts")
+      response <- httpLbs request manager
+      responseStatus response `shouldBe` status200
+      responseBody response `shouldBe` "[{\"accountId\":1,\"accountName\":\"Alice\"},{\"accountId\":2,\"accountName\":\"Bob\"}]"
 
 main :: IO ()
 main = hspec spec
