51c4b6fae595a0f3690981c616e742709514149a
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-12-15 00:36:45 -0300

    Add a test for accounts endpoint

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,5 +19,18 @@ spec = describe "Server" $ do
         responseStatus response `shouldBe` status200
         responseBody response `shouldBe` "\"OK\""
 
+  it "respond with 200 and list of accounts on /accounts" $ do
+    let testConfig = Config {port = 8080, environment = "test"}
+    logOptions <- logOptionsHandle stderr True
+    withLogFunc logOptions $ \logFunc -> do
+      let testApp = App {appLogFunc = logFunc, config = testConfig}
+      testWithApplication (pure $ app testApp) $ \port' -> do
+        manager <- newManager defaultManagerSettings
+        request <- parseRequest ("http://localhost:" <> show port' <> "/accounts")
+        response <- httpLbs request manager
+        responseStatus response `shouldBe` status200
+        -- Assuming the response body is a JSON array of accounts
+        responseBody response `shouldBe` "[{\"accountId\":1,\"accountName\":\"Alice\"},{\"accountId\":2,\"accountName\":\"Bob\"}]"
+
 main :: IO ()
 main = hspec spec
